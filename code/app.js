8de4335e1e2b3a0a7666b20bbfc31064cb05edf0
Mario Parra <mparram@gmail.com>
2025-04-28 10:43:19 +0200

    change bitrate

--- code/app.js
+++ code/app.js
@@ -1,25 +1,33 @@
 const io = require("socket.io-client");
 const { spawn } = require("child_process");
-var cliport = process.env.CLI_PORT || 8081;
-var videodevice = process.env.VIDEO_DEVICE || "video4";
+var cliport = process.env.CLI_PORT || 8080;
+var videodevice = process.env.VIDEO_DEVICE || "video0";
 var connectorsvc= process.env.CONNECTOR_SVC || "localhost";
-const socket = io('http://' + connectorsvc + ':' + cliport, { query: { id: 'cam' } });
-const ffmpeg = spawn("ffmpeg", [
-  "-f", "v4l2",
-  "-framerate", "30",
-  "-s", "854x480",
-  "-i", "/dev/" + videodevice,
-  "-f", "mpegts",
-  "-codec:v", "mpeg1video",
-  "-b:v", "5000k",
-  "-bf", "0",
-  "pipe:1"
-]);
+const socket = io('http://' + connectorsvc + ':' + cliport, { extraHeaders: { origin: 'cam' } });
 
-ffmpeg.stderr.on("data", (data) => {
-  console.error(`FFmpeg error: ${data}`);
-});
-ffmpeg.stdout.on("data", (data) => {
-  socket.emit("video", data);
-});
+var ffmpeg_process = function(){
+  const ffmpeg = spawn("ffmpeg", [
+    "-f", "v4l2",
+    "-framerate", "30",
+    "-s", "854x480",
+    "-i", "/dev/" + videodevice,
+    "-f", "mpegts",
+    "-codec:v", "mpeg1video",
+    "-b:v", "2000k",
+    "-bf", "0",
+    "pipe:1"
+  ]);
 
+  ffmpeg.stdout.on("data", (data) => {
+    socket.emit("video", data);
+  });
+  ffmpeg.stderr.on("data", (data) => {
+    console.error(`FFmpeg error: ${data}`);
+  });
+  ffmpeg.on('exit', function(code) {
+      console.log('child process exited with code ' + code);
+      var timeout = setTimeout(ffmpeg_process, 1000);
+
+  });
+};
+setTimeout(ffmpeg_process, 1000);
